90a89949060c04fea6ef7a3895cf08f5347e1e8f
Jiwuuuuu <milosz.tominski@outlook.com>
2025-07-20 16:03:54 +0200

    added db initialization

--- models/init.go
+++ models/init.go
@@ -1,1 +1,40 @@
 package models
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+func InitDB(driver, filepath string) (*sql.DB, error) {
+	db, err := sql.Open(driver, filepath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open a connection to the database: %w", err)
+	}
+
+	statements := []string{
+		`
+		CREATE TABLE IF NOT EXISTS messages (
+			messageID INTEGER PRIMARY KEY AUTOINCREMENT,
+			roomID INTEGER,
+			timestamp INTEGER,
+			content TEXT NOT NULL,
+			checksum TEXT NOT NULL,
+			username TEXT NOT NULL,
+		);
+		`,
+		`
+		CREATE TABLE IF NOT EXISTS rooms (
+			roomID INTEGER PRIMARY KEY AUTOINCREMENT,
+			name TEXT NOT NULL,
+		);
+		`,
+	}
+
+	for _, s := range statements {
+		if _, err := db.Exec(s); err != nil {
+			return nil, fmt.Errorf("failed to create the table: %w", err)
+		}
+	}
+
+	return db, nil
+}
